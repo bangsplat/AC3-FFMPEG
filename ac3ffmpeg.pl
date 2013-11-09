9cdb05c68804839001e6c0977066b4a9980a5f07
Bangsplat <theron@dankerton.net>
2013-11-09 15:39:48 -0800

    initial commit

---
+++ ac3ffmpeg.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use strict;	# Enforce some good programming rules
+
+#
+# AC3-FFMPEG
+# 
+# use ffmpeg to encode AC-3 files
+# 
+# written by Theron Trowbridge
+# http://therontrowbridge.com
+# 
+# version 0
+# created 2013-11-09
+# modified 2013-11-09
+# 
+
+
+
+
+
+
+
+
